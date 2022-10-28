1a93d88c9ea08a4268558c6a9653a7a38b977726
Korak Ray <korak.ray@columbia.edu>
2022-10-28 13:20:44 -0400

    Run truncate

--- test.py
+++ test.py
@@ -12,45 +12,58 @@ from analyse.acf import gen_mc_acf
 from plot.acf_plot import plot_acf, plot_acf_residuals
 import numpy as np
 from tqdm import tqdm
+import pickle
 
-for snr in [9.0, 3.0, 6.0]:
+for snr in [9.0]:#, 3.0, 6.0]:
 	print(snr)
 	simulations = [simulate_reg,simulate_static2,simulate_dynamic2,simulate_static3,simulate_dynamic3]
 	dataset = ['reg','static2','dynamic2','static3','dynamic3']
 
-	analysis = [analyze_consensusHMM, analyze_ebHMM, analyze_vbHMM_GMM]
-	model = ['consensus', 'eb', 'vbHMM+GMM']
+	analysis = [ analyze_vbHMM_GMM, analyze_consensusHMM, analyze_ebHMM]
+	model = ['vbHMM+GMM','consensus', 'eb']
 
 	nstates = 2
 
 	nrestarts = 100
 	prop = np.array([0.25,0.75])
-
-	for j in range(3):
+	Edict = {}
+	resdict = {}
+	for j in range(10):
+		tau = 1000
+		N = 200
+		truncate = 50*(j+1)
 		sim_dataset = simulations[0]
 		ds = dataset[0]
 		print(ds)
-		analyse = analysis[j]
-		model_used = model[j]
+		analyse = analysis[2]
+		model_used = model[2]
 		print(model_used)
+		print(truncate)
+		ress = []
 		Es = []
 		for i in tqdm(range(nrestarts)):
-			traces,vits,chains= sim_dataset(i,nrestarts,200,1000,snr)
+			traces,vits,chains= sim_dataset(i,nrestarts,N,tau,snr,truncate=truncate)
 
 			res = analyse(traces, nstates)
 			#print(res.mean, res.var, res.frac, res.tmatrix)
 			res.tmstar = res.tmatrix.copy()
 			for i in range(res.tmstar.shape[0]):
 				res.tmstar[i] /= res.tmstar[i].sum()
+			#print(res.var)
 
-			t_res,E_y0yt_res = gen_mc_acf(1,100,res.tmstar,res.mean,res.var,res.frac)
+			t_res,E_y0yt_res = gen_mc_acf(1,1000,res.tmstar,res.mean,res.var,res.frac)
 			Es.append(E_y0yt_res)
+			ress.append(res)
+		Edict[truncate] = Es
+		resdict[truncate] = ress
 
+		pickle.dump(Edict, open( "ACFdicts_{}_{}_{}_{}_PB.p".format(nstates,ds,snr,model_used), "wb" ))
+		pickle.dump(resdict, open( "Resultdicts_{}_{}_{}_{}_PB.p".format(nstates,ds,snr,model_used), "wb" ) )
 		#print(res.mean)
 		#print(res.vb_means)
-		ylims = (-0.2, 0.4)
-		plot_acf(t_res, Es, ds, 'TM', '{}({})'.format(model_used,nstates), snr, pb='NoPB', prop=prop, ylims=ylims)
-		plot_acf_residuals(t_res, Es, ds, 'TM', '{}({})'.format(model_used,nstates), snr, pb='NoPB', prop=prop, ylims=ylims)
+		#ylims = (-0.02, 0.04)
+		#plot_acf(t_res, Es, ds, 'TM', 'new{}({})'.format(model_used,nstates), snr, pb='NoPB', prop=prop)
+		#plot_acf_residuals(t_res, Es, ds, 'TM', 'new{}({})'.format(model_used,nstates), snr, pb='NoPB', prop=prop, ylims=ylims)
 
 
 
